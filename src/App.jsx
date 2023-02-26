934b7d490354826d470ffd7f100dad20d891aead
Petr Kolarik <kolarik.petr@outlook.com>
2023-02-26 14:35:51 +1100

    react-table confing, dummy data, columns

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,48 @@
-import { Fragment, useState } from 'react'
-
+import { Fragment, useMemo, useState } from 'react';
+import { useTable } from 'react-table';
 import { GlobalStyle } from './components/styles/global';
 
+import table_dummy_data from './utils/MOCK_DATA.json';
+
 function App() {
+  const data = useMemo(() => table_dummy_data, []);
+  const columns = useMemo(() => [
+    {
+      Header: "ID",
+      accessor: "id"
+    },
+    {
+      Header: "First Name",
+      accessor: "first_name"
+    },
+    {
+      Header: "Last Name",
+      accessor: "last_name"
+    },
+    {
+      Header: "Email",
+      accessor: "email"
+    },
+    {
+      Header: "Gender",
+      accessor: "gender"
+    },
+    {
+      Header: "Phone",
+      accessor: "phone"
+    },
+    {
+      Header: "Address",
+      accessor: "address"
+    },
+    {
+      Header: "City",
+      accessor: "city"
+    },
+  ], []);
+
+  // destructuring useTable Hook
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data });
 
   return (
     <Fragment>
